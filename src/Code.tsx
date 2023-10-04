31b7f83fc9cf007231018db7cbcee22739a1ef3b
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-10-04 02:29:37 -0600

    :sparkles: Load colors from a HighlighterStyle

--- src/Code.tsx
+++ src/Code.tsx
@@ -1,23 +1,16 @@
 import {Shape, ShapeProps, computed, initial, signal} from '@motion-canvas/2d';
 import {SignalValue, SimpleSignal, useLogger} from '@motion-canvas/core';
-import {classHighlighter, highlightTree, tags} from '@lezer/highlight';
+import {highlightTree} from '@lezer/highlight';
 import {Parser} from '@lezer/common';
 import {parser as jsParser} from '@lezer/javascript';
+import {HighlightStyle} from '@codemirror/language';
+import {Extension} from '@codemirror/state';
 
 export type CodePoint = [number, number];
 export type CodeRange = [CodePoint, CodePoint];
 
-export type Theme = {
-  [K in keyof typeof tags as Exclude<
-    K,
-    'toString' | 'prototype' | 'length'
-  >]?: string;
-} & {
-  default: string;
-};
-
 export interface CodeProps extends ShapeProps {
-  theme: SignalValue<Theme>;
+  style?: SignalValue<HighlightStyle | Extension>;
   parser?: SignalValue<Parser>;
   dialect?: SignalValue<string>;
   code?: SignalValue<string>;
@@ -33,7 +26,23 @@ export class Code extends Shape {
   public declare readonly dialect: SimpleSignal<string, this>;
 
   @signal()
-  public declare readonly theme: SimpleSignal<Theme, this>;
+  public declare readonly style: SimpleSignal<HighlightStyle, this>;
+
+  // TODO: Figure out if we can go from Extension -> HighlightStyle
+  // @computed()
+  // protected highlightStyle(): HighlightStyle {
+  //   const rawStyle = this.style();
+  //   if (!(rawStyle instanceof HighlightStyle)) {
+  //     debugger;
+  //     if (Array.isArray(rawStyle)) {
+  //       rawStyle.find(
+  //         extension => (extension as any)?.value instanceof StyleModule,
+  //       );
+  //     }
+  //   } else {
+  //     return rawStyle;
+  //   }
+  // }
 
   @signal()
   public declare readonly code: SimpleSignal<string, this>;
@@ -45,22 +54,21 @@ export class Code extends Shape {
 
   @computed()
   protected highlighted() {
-    const tokens: {token: string; color: string; classes: string[]}[] = [];
+    const tokens: {token: string; color: string}[] = [];
     highlightTree(
       this.parsed(),
-      classHighlighter,
+      this.style(),
       (from: number, to: number, classList: string) => {
-        const classes = classList.split(' ').map(c => c.split('tok-')[1]);
-        const cls = classes.find((c): c is keyof Theme => c in this.theme());
-        if (!cls) {
-          useLogger().warn(
-            `Unknown theme class${
-              classes.length > 1 ? 'es' : ''
-            } '${classes.join(', ')}'`,
-          );
-        }
+        const rule = this.style()
+          .module.getRules()
+          .split('\n')
+          .find(rule => rule.includes(classList));
+        console.log(this.code().substring(from, to) + ' ' + rule);
+        const color = rule.split('color:')[1]?.split(';')[0];
 
-        const color = this.theme()[cls ?? 'default'];
+        if (!color) {
+          useLogger().warn(`Unknown theme class '${classList}'`);
+        }
 
         // Make sure that all of the characters make it into the list, even
         // if they don't make it through the parser. That shouldn't happen,
@@ -68,15 +76,13 @@ export class Code extends Shape {
         while (tokens.length < to) {
           tokens.push({
             token: this.code().substring(tokens.length, tokens.length + 1),
-            color: this.theme().default,
-            classes: [],
+            color: 'red',
           });
         }
 
         // Update the existing tokens with the new color and classes.
         for (let i = from; i < to; i++) {
           tokens[i].color = color;
-          tokens[i].classes.push(...classes);
         }
       },
     );
